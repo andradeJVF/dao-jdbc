adbd2f9dae4dfc152920a17db6d75adcf3b4b3cc
andradeJVF <joao.felixandrade1201@gmail.com>
2022-09-20 15:12:10 -0300

    Creating Department and Seller

---
+++ src/application/Program.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Department dp = new Department(1, "Books");
+		System.out.println(dp);
+		
+		Seller s1 = new Seller(1, "João", "joao@email.com", sdf.parse("12/01/1999"), 3000.0, dp);
+		System.out.println(s1);
+
+	}
+
+}
