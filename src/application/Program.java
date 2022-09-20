ae2762fabde46699084e80edcc02c32000690ceb
andradeJVF <joao.felixandrade1201@gmail.com>
2022-09-20 15:24:36 -0300

    Creating classes Dao

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,8 @@ public class Program {
 		
 		Department dp = new Department(1, "Books");
 		System.out.println(dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller s1 = new Seller(1, "João", "joao@email.com", sdf.parse("12/01/1999"), 3000.0, dp);
 		System.out.println(s1);
